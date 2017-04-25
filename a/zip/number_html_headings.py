6fb0ec3ae6ede9eb9380f45e743c2da3d0e90189
Jeremy Tammik <jeremy.tammik@autodesk.com>
2017-04-25 13:53:36 +0200

    implemnented number_html_headings.py

---
+++ a/zip/number_html_headings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#
+# number_html_headings.py - add numbering to HTML headings
+#
+# Copyright (C) 2017-04-25 by Jeremy Tammik, Autodesk Inc. 
+#
+import re
+
+filename = '/a/doc/revit/tbc/git/a/zip/Revit_Platform_API_Changes_and_Additions_2018_03.htm'
+
+nlevels = 10 # max heading level to number
+
+ih = [] # current count for each heading level
+
+for i in range(nlevels):
+  ih.append(0)
+  
+def filereadlines( filename ):
+  f = open( filename )
+  data = f.readlines()
+  f.close()
+  return data
+
+if __name__ == '__main__':
+
+  def main():
+    lines = filereadlines( filename )
+    for line in lines:
+      line = line.strip()
+      a = ''
+      if line.startswith('<h'):
+        level = int(line[2])
+        a = '.'.join( [str(ih[l]) for l in range(level)] )
+        a = '<a name="' + a + '"></a>'
+        ih[level] = ih[level] + 1
+      print a + line
+
+main()
+
